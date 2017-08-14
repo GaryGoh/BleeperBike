5c6f7504100fac8e4f2908bc666b78578c1471d2
GaryGoh <gaomingqian90929@gmail.com>
2017-08-14 23:20:55 +0100

    20170815 - done changes

--- app/assets/javascripts/custom/desktop_addedon_js.js
+++ app/assets/javascripts/custom/desktop_addedon_js.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     window.onscroll = function () {
         var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
 //            console.log(scrollTop);
-        if (scrollTop >= 1650) {
+        if (scrollTop >= 850) {
             $('.lineBox>li>p').show(100);
             $('.led_cover').show(700);
             $('.brake_cover').show(700);
